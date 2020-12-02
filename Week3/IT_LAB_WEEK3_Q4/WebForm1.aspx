00bf7a024890467623521a16d10904b4a42a9595
HarshavardhanK <harshavardhan816@gmail.com>
2020-12-02 13:38:41 +0530

    Q4

---
+++ Week3/IT_LAB_WEEK3_Q4/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Captcha.aspx.cs" Inherits="Captcha.Captcha" %> 
+
+ 
+
+<!DOCTYPE html> 
+
+ 
+
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+
+<head runat="server"> 
+
+    <title></title> 
+
+</head> 
+
+<body> 
+
+    <form id="form1" runat="server"> 
+
+        <div> 
+
+            <img runat="server" id="CaptchaImage" alt="Captcha Image" style="width:180px; height:30px" /> 
+
+            <br /><br /> 
+
+            <asp:Button runat="server" ID="Reload" Text="Reload this Captcha" /> 
+
+            <br /> 
+
+            <br /> 
+
+            <asp:TextBox ID="CaptchaText" runat="server" /> 
+
+            <asp:Button runat="server" ID="Submit" Text="Submit Captcha" OnClick="Check_Captcha" /> 
+
+        </div> 
+
+        <br /> 
+
+        <asp:Label Text="Waiting for Verification..." runat="server" ID="VerifiedStatus" /> 
+
+    </form> 
+
+</body> 
+
+</html> 
---
+++ Week3/IT_LAB_WEEK3_Q4/WebForm1.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+using System.Web.UI;
+
+using System.Web.UI.WebControls;
+
+
+
+namespace Captcha
+
+{
+
+    public partial class Captcha : System.Web.UI.Page
+
+    {
+
+        public List<String> captchas;
+
+        public List<String> values;
+
+        public static int currentShow;
+
+        public static int oldShow;
+
+        public static int tries = 3;
+
+        public void Initialize_Lists()
+
+        {
+
+            captchas = new List<String>();
+
+            captchas.Add("https://captcha.com/images/captcha/botdetect3-captcha-ancientmosaic.jpg");
+
+            captchas.Add("https://captcha.com/images/captcha/botdetect3-captcha-blackoverlap.jpg");
+
+            captchas.Add("https://captcha.com/images/captcha/botdetect3-captcha-meltingheat.jpg");
+
+            captchas.Add("https://captcha.com/images/captcha/botdetect3-captcha-negative.jpg");
+
+
+
+            values = new List<String>();
+
+            values.Add("W93BX");
+
+            values.Add("RBSKW");
+
+            values.Add("JA3V8");
+
+            values.Add("459CT");
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+
+        {
+
+            if (tries > 0)
+
+            {
+
+                CaptchaText.Enabled = true;
+
+                Submit.Enabled = true;
+
+            }
+
+            else
+
+            {
+
+                VerifiedStatus.Text = "Max Attempts Exceeded.";
+
+                CaptchaText.Enabled = false;
+
+                Submit.Enabled = false;
+
+            }
+
+
+
+            Random random = new Random();
+
+            oldShow = currentShow;
+
+            currentShow = random.Next(0, 4);
+
+
+
+            Initialize_Lists();
+
+            CaptchaImage.Src = captchas[currentShow];
+
+        }
+
+
+
+        protected void Check_Captcha(object sender, EventArgs e)
+
+        {
+
+            String captchaEntered = CaptchaText.Text.Trim().ToUpper();
+
+            if (captchaEntered.Equals(values[oldShow]))
+
+            {
+
+                VerifiedStatus.Text = "Successfully Verified.";
+
+            }
+
+            else
+
+            {
+
+                tries--;
+
+                VerifiedStatus.Text = "Failed. Remaining Attempts: " + tries;
+
+            }
+
+        }
+
+    }
+
+}
